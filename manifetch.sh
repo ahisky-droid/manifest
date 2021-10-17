266ba5c11f8d53524d1c115a5c983eb3532802bb
alpha2x <hexagonwin@disroot.org>
2021-10-17 23:53:54 +0900

    Edit and fix bug
    Signed-off-by: alpha2x <hexagonwin@disroot.org>

--- manifetch.sh
+++ manifetch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "ManiFetch v1.0 by HexagonWin"
-echo "Target device is $1, on Android $2."
-echo "Attempt to download manifest from raw.githubusercontent.com/ahisky-droid/manifest/$1/$2/roomservice.xml"
+echo "ManiFetch v1.1 by HexagonWin"
+echo "What is your device? (Example : f320)"
+read device
+echo "What's the android version that you're gonna build? (Example : 11)"
+read version
+echo "Target device is $device, on Android $version."
+echo "Attempt to download manifest from raw.githubusercontent.com/ahisky-droid/manifest/$device/$version/roomservice.xml"
 mkdir -p .repo/local_manifests
 cd .repo/local_manifests
-wget https://raw.githubusercontent.com/ahisky-droid/manifest/$1/$2/roomservice.xml
+wget https://raw.githubusercontent.com/ahisky-droid/manifest/$device/$version/roomservice.xml
 cd ../../
 echo "Done! Now run repo sync :)"
 echo "Happy building!"
